79156649159a1f374073e2741a7ec3d8c1ea4892
Sally MacFarlane <sally.macfarlane@consensys.net>
2019-04-15 11:48:43 +1000

    added SimpleVoter

---
+++ contracts/SimpleVoter.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract SimpleVoter {
+
+    mapping( address => bool ) hasVoted;
+    uint256 public yeses;
+    uint256 public nos;
+
+    function castVote( bool _vote ) public {
+
+        require( hasVoted[msg.sender] != true, "Already voted!" );
+
+        if (_vote) {
+            yeses++;
+        } else {
+            nos++;
+        }
+        hasVoted[msg.sender] = true;
+
+    }
+
+}
